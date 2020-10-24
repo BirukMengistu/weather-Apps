0c45c3d783d77575774e6008f45255d477dccd9c
Biruk  Mengistu <biruk.mengistu@learnet.se>
2020-10-24 15:05:14 +0200

    modefied css part and add use urlbase icon

--- view/weatherView.js
+++ view/weatherView.js
@@ -8,10 +8,13 @@ class View {
         for (let i = 0; i < weather.list.length; i++) {
             const icon = weather.list[i].weather[0].icon;
             htmldiv += '<div> ';
-            htmldiv += '<span> <p>Humdity ---> ' + weather.list[i].main.humidity + '%   </p>' + '</span>';
-            htmldiv += '<span> <p>Weather ---> ' + weather.list[i].weather[0].main + '  </p>' + '</span>';
-            htmldiv += '<div id="icon"><img id="wicon" src="img/icons/' + icon + '.png"alt="Weather icon"></div>'
-            htmldiv += '<span><p>Temp -->' + this.weatherConverter(weather.list[i].main.temp) + 'C </p></span>';
+            console.log(weather.list[i].weather[0].icon)
+
+            htmldiv += '<span> <p>Humdity - ' + weather.list[i].main.humidity + '%   </p>' + '</span>';
+            htmldiv += '<span> <p>Weather type -  ' + weather.list[i].weather[0].main + '  </p>' + '</span>';
+            //htmldiv += '<div id="icon"><img id="wicon" src="img/icons/' + icon + '.png"alt="Weather icon"></div>'
+            htmldiv += '<div id="icon"><img id="wicon" src="http://openweathermap.org/img/w/' + icon + '.png"alt="Weather icon"></div>'
+            htmldiv += '<span><p>Temp -' + this.weatherConverter(weather.list[i].main.temp) + '<sup>o </p></span>';
             htmldiv += '<span> City ' + weather.list[i].name + '</span></div>';
 
 
